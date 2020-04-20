d92d894a03dfe35b53678027c4283f9a0f61e7b0
yining24 <ac5230@hotmail.com>
2020-04-20 23:24:46 +0800

    refactor UI

--- app/src/main/java/com/angela/lollipoptest/BindingAdapter.kt
+++ app/src/main/java/com/angela/lollipoptest/BindingAdapter.kt
@@ -6,23 +6,9 @@ import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.RecyclerView
-import com.angela.lollipoptest.data.News
-import com.angela.lollipoptest.data.NewsResult
 import com.angela.lollipoptest.network.LoadApiStatus
 import com.bumptech.glide.request.RequestOptions
 
-
-//@BindingAdapter("homeData")
-//fun bindRecyclerViewWithHomeItems(recyclerView: RecyclerView, homeData: List<NewsResult>?) {
-//    homeData?.let {
-//        recyclerView.adapter?.apply {
-//            when (this) {
-//                is HomePagingAdapter -> submitList(it)
-//            }
-//        }
-//    }
-//}
 
 /**
  * Uses the Glide library to load an image by URL into an [ImageView]
@@ -35,8 +21,8 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
             .load(imgUri)
             .apply(
                 RequestOptions()
-                    .placeholder(R.drawable.ic_launcher_background)
-                    .error(R.drawable.ic_launcher_background)
+                    .placeholder(R.color.transparent)
+                    .error(R.color.transparent)
             )
             .into(imgView)
     }
